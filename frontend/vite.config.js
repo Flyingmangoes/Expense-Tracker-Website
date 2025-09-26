809e69cb6014af251529abf03b5a7d2c6c1f48c0
floatingmangoes <zerotwo02e@gmail.com>
2025-09-26 14:27:30 +0800

    Initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import { resolve } from 'path';
+
+export default defineConfig({
+    root: '.',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'pages/login.html'),
+                register: resolve(__dirname, 'pages/register.html'),
+                about: resolve(__dirname, 'pages/aboutus.html')
+            }
+        }
+    }
+})
+
